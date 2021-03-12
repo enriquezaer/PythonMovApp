934585da9f83ac6ef4acb6da35c4232026c019e5
Alex enriquez <Alex@gmail.com>
2021-03-11 23:32:22 -0600

    despues

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class SignUpScreen(Screen):
 
         with open('users.json', 'w') as file:
              json.dump(users,file)
+        print(users)     
         self.manager.current = "sign_up_screen_success"
 
 class SignUpScreenSuccess(Screen):
